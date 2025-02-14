046899632e6bfaa438f3d2a7bbc710ae15708d72
Fakhar Abbas Bhatti <fakharabbasbhatti333@gmail.com>
2025-02-14 10:50:32 +0500

    first commit

---
+++ forloop.js
@@ -0,0 +1,103 @@
+// for (let i = 0; i < 5; i++) {
+//     console.log(i);
+// }
+// const array = ['apple', 'banana', 'cherry'];
+// for (let i = 0; i < array.length; i++) {
+//     console.log(array[i]);
+// }
+
+
+
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// console.log(4)
+// console.log(5)
+// console.log( 333)
+
+
+
+//  for(let num=0; num<100;num++){
+//     console.log(num);
+// }
+
+
+for(let num=60; num>100;num++){
+    console.log(num)
+}
+
+// let =0;
+// do{
+//     console.log(i);
+//     i++;}
+//     while(i<5);
+
+//     do{
+//         input=prompt('enter an number greater than 5.'); }
+//    while(input<=5);
+
+
+
+//    let t=0;
+//    do{t++;}
+// i f (i===3)
+// {continue; console.log(i);}
+// while(i<5);
+
+
+// count=5
+// while count>0;
+// print (count)
+// count<=1
+print(" ")
+
+let age = 20;
+if (age>=18 )
+{
+    console.log('you are adult.')
+}
+
+
+for (let i = 0; i < 5; i++) {
+    console.log(" " + (i + 1));
+  }
+  
+
+  const fruits = ["Apple", "Banana", "Orange", "Mango",];
+  for (let i = 0; i < fruits.length; i++) {
+    console.log(" " + fruits[i]);
+  }
+  
+ 
+  for (let i = 5; i > 0; i--) {
+    console.log(i);
+  }
+
+
+
+
+
+  let x=10;
+  let y=20;
+  if(x>15)
+  {
+if (x>25){
+    console.log('both conditions true');
+} }
+
+
+let i=0;
+do{
+    console.log(i);
+i++;
+
+}while (i<0);
+
+
+let u=0;
+while(u<5){
+    console.log(u);
+    u++;
+}
+
+ 
